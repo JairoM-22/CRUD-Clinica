2204857a546b9f80204243ccf06d110f610225c6
Jcontrerasm <126794784+PachitoBott@users.noreply.github.com>
2026-06-25 18:21:01 -0500

    todos GET y DELETE listos

--- src/server.ts
+++ src/server.ts
@@ -14,10 +14,12 @@ app.use(express.json())
 
 // GETTERS
 // Pacientes
-app.get("/api/paciente_clone", async (_req, res) => {
+
+//GET
+app.get("/api/paciente", async (_req, res) => {
   try {
     const { rows } = await pool.query<Paciente>(
-      "SELECT * FROM paciente_clone ORDER BY id ASC"
+      "SELECT * FROM paciente ORDER BY id ASC"
     )
     res.json(rows)
   } catch (error) {
@@ -25,10 +27,9 @@ app.get("/api/paciente_clone", async (_req, res) => {
     res.status(500).json({ error: "Error al obtener los pacientes" })
   }
 })
-//asdas
 
-
-app.delete("/api/paciente_clone", async (req, res) => {
+//DELETE
+app.delete("/api/paciente", async (req, res) => {
   const { id } = req.body as { id: number}
 
   if (!id ) {
@@ -38,7 +39,7 @@ app.delete("/api/paciente_clone", async (req, res) => {
 
   try {
     const { rows } = await pool.query<Paciente>(
-      `DELETE FROM paciente_clone
+      `DELETE FROM paciente
        WHERE ID = $1
        RETURNING *`,
        [id || "Anonimo"]
@@ -52,10 +53,11 @@ app.delete("/api/paciente_clone", async (req, res) => {
 })
 
 // Cita
+//GET
 app.get("/api/cita", async (_req, res) => {
   try {
     const { rows } = await pool.query<Cita>(
-      "SELECT * FROM cita ORDER BY id DESC"
+      "SELECT * FROM cita ORDER BY id ASC"
     )
     res.json(rows)
   } catch (error) {
@@ -63,12 +65,39 @@ app.get("/api/cita", async (_req, res) => {
     res.status(500).json({ error: "Error al obtener las citas" })
   }
 })
+//DELETE
+
+app.delete("/api/cita", async (req, res) => {
+  const { id } = req.body as { id: number}
+
+  if (!id ) {
+    res.status(400).json({ error: "El campo 'id' es requerido" })
+    return
+  }
+
+  try {
+    const { rows } = await pool.query<Cita>(
+      `DELETE FROM cita
+       WHERE ID = $1
+       RETURNING *`,
+       [id || "Anonimo"]
+    )
+
+    res.status(201).json(rows[0])
+  } catch (error) {
+    console.error("Error al crear mensaje:", error)
+    res.status(500).json({ error: "Error al crear el mensaje" })
+  }
+})
+
+
 
 // Doctor
+//GET
 app.get("/api/doctor", async (_req, res) => {
   try {
     const { rows } = await pool.query<Doctor>(
-      "SELECT * FROM doctor ORDER BY id DESC"
+      "SELECT * FROM doctor ORDER BY id asc"
     )
     res.json(rows)
   } catch (error) {
@@ -77,11 +106,38 @@ app.get("/api/doctor", async (_req, res) => {
   }
 })
 
+//DELETE
+
+app.delete("/api/doctor", async (req, res) => {
+  const { id } = req.body as { id: number}
+
+  if (!id ) {
+    res.status(400).json({ error: "El campo 'id' es requerido" })
+    return
+  }
+
+  try {
+    const { rows } = await pool.query<Doctor>(
+      `DELETE FROM doctor
+       WHERE ID = $1
+       RETURNING *`,
+       [id || "Anonimo"]
+    )
+
+    res.status(201).json(rows[0])
+  } catch (error) {
+    console.error("Error al crear mensaje:", error)
+    res.status(500).json({ error: "Error al crear el mensaje" })
+  }
+})
+
+
 // Especialidad
+//GET
 app.get("/api/especialidad", async (_req, res) => {
   try {
     const { rows } = await pool.query<Especialidad>(
-      "SELECT * FROM especialidad ORDER BY id DESC"
+      "SELECT * FROM especialidad ORDER BY id ASC"
     )
     res.json(rows)
   } catch (error) {
@@ -90,7 +146,32 @@ app.get("/api/especialidad", async (_req, res) => {
   }
 })
 
+//DELETE
+app.delete("/api/especialidad", async (req, res) => {
+  const { id } = req.body as { id: number}
+
+  if (!id ) {
+    res.status(400).json({ error: "El campo 'id' es requerido" })
+    return
+  }
+
+  try {
+    const { rows } = await pool.query<Especialidad>(
+      `DELETE FROM especialidad
+       WHERE ID = $1
+       RETURNING *`,
+       [id || "Anonimo"]
+    )
+
+    res.status(201).json(rows[0])
+  } catch (error) {
+    console.error("Error al crear mensaje:", error)
+    res.status(500).json({ error: "Error al crear el mensaje" })
+  }
+})
+
 // Especialista
+//GET
 app.get("/api/especialista", async (_req, res) => {
   try {
     const { rows } = await pool.query<Especialista>(
@@ -102,8 +183,33 @@ app.get("/api/especialista", async (_req, res) => {
     res.status(500).json({ error: "Error al obtener los especialistas" })
   }
 })
+//DELETE
+app.delete("/api/especialista", async (req, res) => {
+  const { id } = req.body as { id: number}
+
+  if (!id ) {
+    res.status(400).json({ error: "El campo 'id' es requerido" })
+    return
+  }
+
+  try {
+    const { rows } = await pool.query<Especialista>(
+      `DELETE FROM especialista
+       WHERE ID = $1
+       RETURNING *`,
+       [id || "Anonimo"]
+    )
+
+    res.status(201).json(rows[0])
+  } catch (error) {
+    console.error("Error al crear mensaje:", error)
+    res.status(500).json({ error: "Error al crear el mensaje" })
+  }
+})
+
 
 // Receta
+//GET
 app.get("/api/receta", async (_req, res) => {
   try {
     const { rows } = await pool.query<Receta>(
@@ -113,6 +219,29 @@ app.get("/api/receta", async (_req, res) => {
   } catch (error) {
     console.error("Error al listar las recetas:", error)
     res.status(500).json({ error: "Error al obtener las recetas" })
+  }
+})
+//DELETE
+app.delete("/api/receta", async (req, res) => {
+  const { id } = req.body as { id: number}
+
+  if (!id ) {
+    res.status(400).json({ error: "El campo 'id' es requerido" })
+    return
+  }
+
+  try {
+    const { rows } = await pool.query<Receta>(
+      `DELETE FROM Receta
+       WHERE ID = $1
+       RETURNING *`,
+       [id || "Anonimo"]
+    )
+
+    res.status(201).json(rows[0])
+  } catch (error) {
+    console.error("Error al crear mensaje:", error)
+    res.status(500).json({ error: "Error al crear el mensaje" })
   }
 })
 
