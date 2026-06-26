070098ec5e8cc418ad092002634d3710fa3faf45
Jcontrerasm <126794784+PachitoBott@users.noreply.github.com>
2026-06-25 19:10:26 -0500

    PUT LISTOS CASI 90%

--- src/server.ts
+++ src/server.ts
@@ -12,14 +12,13 @@ app.use(express.json())
 
 // ─── API REST ───
 
-// GETTERS
-// Pacientes
 
+// Pacientes
 //GET
-app.get("/api/paciente", async (_req, res) => {
+app.get("/api/paciente_clone", async (_req, res) => {
   try {
     const { rows } = await pool.query<Paciente>(
-      "SELECT * FROM paciente ORDER BY id ASC"
+      "SELECT * FROM paciente_clone ORDER BY id ASC"
     )
     res.json(rows)
   } catch (error) {
@@ -27,7 +26,6 @@ app.get("/api/paciente", async (_req, res) => {
     res.status(500).json({ error: "Error al obtener los pacientes" })
   }
 })
-
 //DELETE
 app.delete("/api/paciente", async (req, res) => {
   const { id } = req.body as { id: number}
@@ -51,6 +49,39 @@ app.delete("/api/paciente", async (req, res) => {
     res.status(500).json({ error: "Error al crear el mensaje" })
   }
 })
+//PUT
+app.put("/api/paciente_clone", async (req, res) => {
+  const { id, nombre, correo, fecha_nacimiento, fecha_registro } = req.body;
+
+  if (!id) {
+    res.status(400).json({ error: "El campo 'id' es requerido" });
+    return;
+  }
+
+  try {
+    const { rows } = await pool.query<Paciente>(
+      `UPDATE paciente_clone
+       SET nombre = $1, correo = $2, fecha_nacimiento = $3, fecha_registro = $4
+       WHERE id = $5
+       RETURNING *`,
+      [nombre, correo, fecha_nacimiento, fecha_registro, id]
+    );
+
+    if (rows.length === 0) {
+      res.status(404).json({ error: "Paciente no encontrado" });
+      return;
+    }
+
+    res.status(200).json(rows[0]);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Error al actualizar" });
+  }
+});
+
+
+
+
 
 // Cita
 //GET
@@ -66,7 +97,6 @@ app.get("/api/cita", async (_req, res) => {
   }
 })
 //DELETE
-
 app.delete("/api/cita", async (req, res) => {
   const { id } = req.body as { id: number}
 
@@ -89,6 +119,38 @@ app.delete("/api/cita", async (req, res) => {
     res.status(500).json({ error: "Error al crear el mensaje" })
   }
 })
+//PUT
+app.put("/api/cita", async (req, res) => {
+  const {id, paciente_id, doctor_id, fecha, hora, motivo_consulta, estado} = req.body;
+
+  if (!id) {
+    res.status(400).json({ error: "El campo 'id' es requerido" });
+    return;
+  }
+
+  try {
+    const { rows } = await pool.query<Cita>(
+      `UPDATE cita
+       SET paciente_id = $1, doctor_id = $2, fecha = $3, hora = $4, motivo_consulta=$5, estado=$6
+       WHERE id = $7
+       RETURNING *`,
+      [paciente_id, doctor_id, fecha, hora, motivo_consulta, estado ,id]
+    );
+
+    if (rows.length === 0) {
+      res.status(404).json({ error: "Cita no encontrada" });
+      return;
+    }
+
+    res.status(200).json(rows[0]);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Error al actualizar" });
+  }
+});
+
+
+
 
 
 
@@ -105,9 +167,7 @@ app.get("/api/doctor", async (_req, res) => {
     res.status(500).json({ error: "Error al obtener los doctores" })
   }
 })
-
 //DELETE
-
 app.delete("/api/doctor", async (req, res) => {
   const { id } = req.body as { id: number}
 
@@ -130,6 +190,38 @@ app.delete("/api/doctor", async (req, res) => {
     res.status(500).json({ error: "Error al crear el mensaje" })
   }
 })
+//PUT
+app.put("/api/doctor", async (req, res) => {
+  const {id, nombre, correo, telefono, numero_consultorio} = req.body;
+
+  if (!id) {
+    res.status(400).json({ error: "El campo 'id' es requerido" });
+    return;
+  }
+
+  try {
+    const { rows } = await pool.query<Doctor>(
+      `UPDATE doctor
+       SET nombre = $1, correo = $2, telefono = $3,numero_consultorio = $4
+       WHERE id = $5
+       RETURNING *`,
+      [nombre, correo, telefono, numero_consultorio,id]
+    );
+
+    if (rows.length === 0) {
+      res.status(404).json({ error: "Doctor no encontrada" });
+      return;
+    }
+
+    res.status(200).json(rows[0]);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Error al actualizar" });
+  }
+});
+
+
+
 
 
 // Especialidad
@@ -145,7 +237,6 @@ app.get("/api/especialidad", async (_req, res) => {
     res.status(500).json({ error: "Error al obtener las especialidades" })
   }
 })
-
 //DELETE
 app.delete("/api/especialidad", async (req, res) => {
   const { id } = req.body as { id: number}
@@ -157,7 +248,7 @@ app.delete("/api/especialidad", async (req, res) => {
 
   try {
     const { rows } = await pool.query<Especialidad>(
-      `DELETE FROM especialidad
+      `DELETE FROM especialidad_clone
        WHERE ID = $1
        RETURNING *`,
        [id || "Anonimo"]
@@ -169,6 +260,39 @@ app.delete("/api/especialidad", async (req, res) => {
     res.status(500).json({ error: "Error al crear el mensaje" })
   }
 })
+//PUT
+app.put("/api/especialidad", async (req, res) => {
+  const {id, nombre, descripcion} = req.body;
+
+  if (!id) {
+    res.status(400).json({ error: "El campo 'id' es requerido" });
+    return;
+  }
+
+  try {
+    const { rows } = await pool.query<Especialidad>(
+      `UPDATE especialidad
+       SET nombre = $1, descripcion = $2
+       WHERE id = $3
+       RETURNING *`,
+      [nombre, descripcion, id]
+    );
+
+    if (rows.length === 0) {
+      res.status(404).json({ error: "Especialidad no encontrada" });
+      return;
+    }
+
+    res.status(200).json(rows[0]);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Error al actualizar" });
+  }
+});
+
+
+
+
 
 // Especialista
 //GET
@@ -206,6 +330,37 @@ app.delete("/api/especialista", async (req, res) => {
     res.status(500).json({ error: "Error al crear el mensaje" })
   }
 })
+//PUT
+app.put("/api/especialista", async (req, res) => {
+  const {id, doctor_id, especialidad_id} = req.body;
+
+  if (!id) {
+    res.status(400).json({ error: "El campo 'id' es requerido" });
+    return;
+  }
+
+  try {
+    const { rows } = await pool.query<Especialista>(
+      `UPDATE especialista
+       SET doctor_id = $1, especialidad_id = $2
+       WHERE id = $3
+       RETURNING *`,
+      [doctor_id, especialidad_id, id]
+    );
+
+    if (rows.length === 0) {
+      res.status(404).json({ error: "Especialista no encontrado" });
+      return;
+    }
+
+    res.status(200).json(rows[0]);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Error al actualizar" });
+  }
+});
+
+
 
 
 // Receta
@@ -244,6 +399,36 @@ app.delete("/api/receta", async (req, res) => {
     res.status(500).json({ error: "Error al crear el mensaje" })
   }
 })
+//PUT
+app.put("/api/receta", async (req, res) => {
+  const {id, cita_id, medicamento, dosis, instruccion, fecha_emision} = req.body;
+
+  if (!id) {
+    res.status(400).json({ error: "El campo 'id' es requerido" });
+    return;
+  }
+
+  try {
+    const { rows } = await pool.query<Receta>(
+      `UPDATE receta
+       SET cita_id = $1, medicamento = $2, dosis = $3, instruccion = $4,fecha_emision = $5
+       WHERE id = $6
+       RETURNING *`,
+      [cita_id, medicamento, dosis, instruccion, fecha_emision, id]
+    );
+
+    if (rows.length === 0) {
+      res.status(404).json({ error: "Receta no encontrada" });
+      return;
+    }
+
+    res.status(200).json(rows[0]);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Error al actualizar" });
+  }
+});
+
 
 
 // ─── Iniciar servidor ───
